c8ef52f3f07619a2c44a55535087f0b9793ff9ce
luisburigo <gugaba29@gmail.com>
2025-08-04 10:05:28 -0300

    chore: update packages names

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -1,22 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import biomePlugin from 'vite-plugin-biome';
-import tailwindcss from '@tailwindcss/vite'
-import path from 'node:path';
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 import TanStackRouterVite from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import biomePlugin from "vite-plugin-biome";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite({
-    target: 'react',
-    autoCodeSplitting: true
-  }), biomePlugin({
-    mode: 'lint',
-    files: '.'
-  }), tailwindcss(), tailwindcss()],
+  plugins: [
+    react(),
+    TanStackRouterVite({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    biomePlugin({
+      mode: "lint",
+      files: ".",
+    }),
+    tailwindcss(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
